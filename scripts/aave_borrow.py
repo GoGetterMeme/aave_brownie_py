cc1bc0afc82bcf6fe3538fa751b12a531485e689
YetiBridge <yetiswap@protonmail.com>
2021-09-19 19:27:48 -0400

    first commit

---
+++ scripts/aave_borrow.py
@@ -0,0 +1,36 @@
+from brownie import network, config, interface, accounts
+from scripts.helpful_scripts import get_account
+from scripts.get_weth import get_weth
+
+
+def main():
+    account = get_account()
+    erc20_address = config["networks"][network.show_active()]["weth_token"]
+    if network.show_active() in ["mainnet-fork"]:
+        get_weth()
+    # ABI
+    # Address
+    lending_pool = get_lending_pool()
+    print(lending_pool)
+
+
+def approve_erc20(amount, spender, erc20_address, account):
+    print("Approving ERC20 token...")
+    erc20 = interface.IERC20(erc20_address)
+    tx = erc20.approve(spender, amount, {"from": account})
+    tx.wait(1)
+    print("Approved")
+    return tx
+    # ABI
+    # Address
+
+
+def get_lending_pool():
+    lending_pool_addresses_provider = interface.ILendingPoolAddressesProvider(
+        config["networks"][network.show_active()]["lending_pool_addresses_provider"]
+    )
+    lending_pool_addresses_provider = lending_pool_addresses_provider.getLendingPool()
+    # ABI
+    # Address - check!
+    lending_pool = interface.ILendingPool(lending_pool_address)
+    return lending_pool
